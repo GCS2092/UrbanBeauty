ed889e6899425c118dc0c2f79462f26fd4414cde
GCS2092 <slovengama05@gmail.com>
2025-11-14 02:15:36 +0000

    Fix frontend directory - add files properly

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-left bg-zinc-50 dark:bg-black">
+      <h1 className="text-4xl font-bold text-white red:text-white">
+        Coucou
+      </h1>
+    </div>
+  );
+}
