017c5db87a48d8e6cbec43450a17414bec9ba316
GCS2092 <slovengama05@gmail.com>
2026-06-20 23:41:10 +0000

    feat: OneSignal PWA notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import OneSignal from 'react-onesignal'
+
+OneSignal.init({
+  appId: 'VOTRE_APP_ID_ICI',
+  notifyButton: { enable: false },
+  allowLocalhostAsSecureOrigin: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
