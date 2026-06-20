636accd3e605606e9d67b126bdfddbe5c2310a93
GCS2092 <slovengama05@gmail.com>
2026-06-20 23:46:26 +0000

    feat: icones PWA + OneSignal app ID via env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+﻿import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import OneSignal from 'react-onesignal'
 
 OneSignal.init({
-  appId: 'VOTRE_APP_ID_ICI',
+  appId: import.meta.env.VITE_ONESIGNAL_APP_ID,
   notifyButton: { enable: false },
   allowLocalhostAsSecureOrigin: true,
 })
